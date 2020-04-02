b41f63fc2f92eeff493345a71098813c29b78f53
danonprice <danonprice@users.noreply.github.com>
2020-04-02 12:18:48 -0700

    web demo

---
+++ web_demo/trackfile.js
@@ -0,0 +1,78 @@
+var wildtracks = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "species": "Puma",
+                "name": "Cassie",
+                "sex": "F",
+                "iconUrl": "images/Test_Data__Puma__F-Cassie__Cassie.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.166694, -14.627139]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Lowland Tapir",
+                "name": "Sorocaba",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Lowland_Tapir__Sorocaba_2.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.126559, -14.619829]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Black Rhino",
+                "name": "Unknown",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Black_Rhino__Unknown.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.139287, -14.672822]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Leopard",
+                "name": "Shakira",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Leopard__Shakira.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.187533, -14.638532]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Leopard",
+                "name": "Shakira",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Leopard__Shakira-1.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.184959, -14.651031]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Leopard",
+                "name": "Shakira",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Leopard__Shakira-2.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.206336, -14.658966]
+            }
+        }
+    ]
+};
