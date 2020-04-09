021bf764818bc6aeea0bef61f6534090c07c18e5
danonprice <danonprice@users.noreply.github.com>
2020-04-08 18:12:42 -0700

    icon change

--- web_demo/trackfile.js
+++ web_demo/trackfile.js
@@ -73,6 +73,69 @@ var wildtracks = {
                 "type": "Point",
                 "coordinates": [26.206336, -14.658966]
             }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Bengal Tiger",
+                "name": "Rajaji",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Bengal_Tiger__Rajaji-19a.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.19002, -14.672822]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Amur Tiger",
+                "name": "Unknown",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Amur_Tiger__Amur-1020-2.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.20001, -14.619829]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "White Rhino",
+                "name": "Kalakwa",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__White_Rhino__6JunKML2a.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.139287, -14.651031]
+
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Cheetah",
+                "name": "Tearmark",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Cheetah__Tearmark.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.139287, -14.638532]
+
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "species": "Cheetah",
+                "name": "Tearmark",
+                "sex": "U",
+                "iconUrl": "images/Test_Data__Cheetah__Tearmark-1.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [26.156603, -14.651031]
+
+            }
         }
     ]
 };
