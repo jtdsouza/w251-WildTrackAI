1dcf3cb430db13ca33528d2c96f944f8b847ff9c
Dan Price <dan.price@berkeley.edu>
2020-04-09 22:53:23 -0700

    json fields added

--- web_demo/trackfile.js
+++ web_demo/trackfile.js
@@ -4,9 +4,13 @@ var wildtracks = {
         {
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-33",
+                "deviceID": "0001",
                 "species": "Puma",
                 "name": "Cassie",
+                "confidence":92,
                 "sex": "F",
+
                 "iconUrl": "images/Test_Data__Puma__F-Cassie__Cassie.jpg"
             },
             "geometry": {
@@ -16,8 +20,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-33",
+                "deviceID": "0001",
                 "species": "Lowland Tapir",
                 "name": "Sorocaba",
+                "confidence":88,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Lowland_Tapir__Sorocaba_2.jpg"
             },
@@ -28,8 +35,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-33",
+                "deviceID": "0001",                
                 "species": "Black Rhino",
                 "name": "Unknown",
+                "confidence":84,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Black_Rhino__Unknown.jpg"
             },
@@ -40,8 +50,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-33",
+                "deviceID": "0001",                
                 "species": "Leopard",
                 "name": "Shakira",
+                "confidence":70,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Leopard__Shakira.jpg"
             },
@@ -52,8 +65,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-33",
+                "deviceID": "0001",                
                 "species": "Leopard",
                 "name": "Shakira",
+                "confidence":93,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Leopard__Shakira-1.jpg"
             },
@@ -64,8 +80,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-33",
+                "deviceID": "0001",                
                 "species": "Leopard",
                 "name": "Shakira",
+                "confidence":98,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Leopard__Shakira-2.jpg"
             },
@@ -76,8 +95,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-33",
+                "deviceID": "0001",                
                 "species": "Bengal Tiger",
                 "name": "Rajaji",
+                "confidence":78,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Bengal_Tiger__Rajaji-19a.jpg"
             },
@@ -88,8 +110,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-33",
+                "deviceID": "0001",                
                 "species": "Amur Tiger",
                 "name": "Unknown",
+                "confidence":87,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Amur_Tiger__Amur-1020-2.jpg"
             },
@@ -100,8 +125,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-45-21",
+                "deviceID": "0001",                
                 "species": "White Rhino",
                 "name": "Kalakwa",
+                "confidence":89,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__White_Rhino__6JunKML2a.jpg"
             },
@@ -113,8 +141,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-56-33",
+                "deviceID": "0001",                
                 "species": "Cheetah",
                 "name": "Tearmark",
+                "confidence":92,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Cheetah__Tearmark.jpg"
             },
@@ -126,8 +157,11 @@ var wildtracks = {
         },{
             "type": "Feature",
             "properties": {
+                "timeStamp": "2020-04-04_20-55-39",
+                "deviceID": "0001",                
                 "species": "Cheetah",
                 "name": "Tearmark",
+                "confidence":94,
                 "sex": "U",
                 "iconUrl": "images/Test_Data__Cheetah__Tearmark-1.jpg"
             },
